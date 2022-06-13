4b0ad56a7f0c3116eda72fa8ec8dd1ae5e781c6b
SebaQuiros <sebaquiros@gmail.com>
2022-06-13 16:32:35 -0300

    experiencia

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Experience } from 'src/app/interfaces/experience';
+
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss'],
 })
 export class ExperienceComponent implements OnInit {
+  experience: Experience = {
+    id: 1,
+    button: 'Fundación IDA',
+    role: 'Voluntario en',
+    title: 'Fundación IDA',
+    subtitle: 'Investigación en Diseño Argentino',
+    time: 'Enero 2021 - Marzo 2022',
+    description: [
+      'Encargado del área de arquitectura para la obtención, recepción y catalogación de material nuevo.',
+      'Investigador para el proyecto "Épica. Hazañas del diseño argentino."',
+    ],
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
---
+++ src/app/interfaces/experience.ts
@@ -0,0 +1,9 @@
+export interface Experience {
+  id: number;
+  button: string;
+  role: string;
+  title: string;
+  subtitle: string;
+  time: string;
+  description: string[];
+}
