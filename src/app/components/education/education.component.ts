0b33800442bfe27154a5b422b36407f18abff0c4
SebaQuiros <sebaquiros@gmail.com>
2022-06-13 17:42:25 -0300

    list

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Education } from 'src/app/interfaces/education';
+import { EDUCATIONS } from 'src/app/mock-educations';
+
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.scss'],
 })
 export class EducationComponent implements OnInit {
+  educations = EDUCATIONS;
   constructor() {}
 
   ngOnInit(): void {}
+
+  selectedEducation: Education = EDUCATIONS[0];
+  onSelect(experience: Education): void {
+    this.selectedEducation = experience;
+  }
 }
---
+++ src/app/interfaces/education.ts
@@ -0,0 +1,10 @@
+export interface Education {
+  id: number;
+  button: string;
+  role: string;
+  title: string;
+  link: string;
+  subtitle: string;
+  time: string;
+  description: string[];
+}
