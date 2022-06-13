6ffab3c74aee71de605dbb4d16d8cc7508d1a5b8
SebaQuiros <sebaquiros@gmail.com>
2022-06-13 01:41:48 -0300

    secciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NetworksComponent } from './components/networks/networks.component';
 import { MailComponent } from './components/mail/mail.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NetworksComponent,
-    MailComponent
+    MailComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProyectsComponent
   ],
   imports: [
     BrowserModule,
