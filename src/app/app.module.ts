60244d861ec70d064d59543a675212ea4e2bc4f7
SebaQuiros <sebaquiros@gmail.com>
2022-06-13 01:08:58 -0300

    networks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { NetworksComponent } from './components/networks/networks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NetworksComponent
   ],
   imports: [
     BrowserModule,
