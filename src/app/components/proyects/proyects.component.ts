e3a5c7194fffad989dc55cff4b6b6152769077f4
SebaQuiros <sebaquiros@gmail.com>
2022-06-14 14:24:32 -0300

    proyects

--- src/app/components/proyects/proyects.component.ts
+++ src/app/components/proyects/proyects.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Proyect } from 'src/app/interfaces/proyect';
+import { PROYECTS } from 'src/app/mock-proyects';
+
 @Component({
   selector: 'app-proyects',
   templateUrl: './proyects.component.html',
-  styleUrls: ['./proyects.component.scss']
+  styleUrls: ['./proyects.component.scss'],
 })
 export class ProyectsComponent implements OnInit {
+  proyects = PROYECTS;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/interfaces/proyect.ts
@@ -0,0 +1,10 @@
+export interface Proyect {
+  id: number;
+  type: string;
+  link: string;
+  title: string;
+  description: string;
+  tools: string[];
+  img: string;
+  imgAlt: string;
+}
