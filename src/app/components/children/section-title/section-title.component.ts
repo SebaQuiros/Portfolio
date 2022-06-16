c3ed4e1013b340c4224dd55de019d94ac50c8606
SebaQuiros <sebaquiros@gmail.com>
2022-06-16 16:17:31 -0300

    listo

--- src/app/components/children/section-title/section-title.component.ts
+++ src/app/components/children/section-title/section-title.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-section-title',
   templateUrl: './section-title.component.html',
-  styleUrls: ['./section-title.component.scss']
+  styleUrls: ['./section-title.component.scss'],
 })
 export class SectionTitleComponent implements OnInit {
+  @Input() number: string = '00.';
+  @Input() text: string = 'Sample';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
