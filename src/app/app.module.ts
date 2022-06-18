335f1cda2bd1a2c3112c1efcd1f4e668a7587f0f
SebaQuiros <sebaquiros@gmail.com>
2022-06-18 16:53:47 -0300

    adsad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -33,7 +34,7 @@ import { EditMenuComponent } from './components/children/edit-menu/edit-menu.com
     SectionTitleComponent,
     EditMenuComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
