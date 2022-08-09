e45507b52d35f59d1659898f2b652f9fa2781885
SebaQuiros <sebaquiros@gmail.com>
2022-08-09 12:36:56 -0300

    a

--- src/app/components/proyects/proyects.component.ts
+++ src/app/components/proyects/proyects.component.ts
@@ -33,7 +33,7 @@ export class ProyectsComponent implements OnInit {
       type: 'Ejemplo',
       link: '',
       title: 'Nuevo proyecto',
-      description: '',
+      description: 'Lorem ipsum',
       img: '',
       imgAlt: '',
     };
