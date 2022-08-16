b41513a1de71642154cb63611c67b2280fe000ab
SebaQuiros <sebaquiros@gmail.com>
2022-08-16 11:22:17 -0300

    deploy

--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -9,9 +9,8 @@ import { Experience } from 'src/app/interfaces/experience';
   providedIn: 'root',
 })
 export class ExperienceService {
-  // private experiencesUrl =
-  // 'https://sebaquirosportfolio-backend.herokuapp.com/experiences';
-  private experiencesUrl = 'http://localhost:8080/experiences';
+  private experiencesUrl =
+    'https://sebaquirosportfolio-backend.herokuapp.com/experiences';
 
   httpOptions = {
     headers: new HttpHeaders({
